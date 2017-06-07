fa346f1081db7b9de85fb25c7053572c1e2fc243
Pierre Frin <piguom@hotmail.fr>
2017-06-07 19:09:22 +0200

    init folder and first frame

--- src/Fenetre/Main.java
+++ src/Fenetre/Main.java
@@ -1,5 +1,22 @@
 package Fenetre;
 
-public class Main {
+import javax.swing.JFrame;
+
+public class Main extends JFrame{
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	private JFrame frame;
+	
+	public Main() {
+
+		frame = new JFrame("Processus stochastiques appliqués au problème de la file d'attente");
+		frame.setSize(1000,600);
+		frame.setLocationRelativeTo(null);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setVisible(true);	
+	}
 
 }
